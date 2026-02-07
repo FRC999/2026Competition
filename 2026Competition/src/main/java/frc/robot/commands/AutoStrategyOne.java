db1ae9b3fbd825c4b17517f7ceee05de8e573bac
Sudhamsu <academyoffice79@gmail.com>
2026-02-07 15:05:25 -0500

    created auto for first auto strategy

--- 2026Competition/src/main/java/frc/robot/commands/AutoStrategyOne.java
+++ 2026Competition/src/main/java/frc/robot/commands/AutoStrategyOne.java
@@ -1,28 +1,42 @@
-// // Copyright (c) FIRST and other WPILib contributors.
-// // Open Source Software; you can modify and/or share it under the terms of
-// // the WPILib BSD license file in the root directory of this project.
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 
-// package frc.robot.commands;
+package frc.robot.commands;
 
-// import edu.wpi.first.wpilibj2.command.DeferredCommand;
-// import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import java.util.Set;
 
-// // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
-// // information, see:
-// // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-// public class AutoStrategyOne extends SequentialCommandGroup {
-//   /** Creates a new AutoStrategyOne. */
-//   public AutoStrategyOne() {
-//     // Add your commands in the addCommands() call, e.g.
-//     // addCommands(new FooCommand(), new BarCommand());
-//     addCommands(
-//       new DeferredCommand(
-//             () -> RobotContainer.runTrajectory2PosesSlow(
-//                 RobotContainer.driveSubsystem.getInitialVisionAidedOdometryPose( new Pose2d(7.219, 6.130, Rotation2d.k180deg)), // if vision is not available at the start, use that pose
-//                 RobotPoseConstants.visionRobotPoses.get("RobotBluReef3Right"),
-//                 false),
-//                 Set.of)
-//             .alongWith(new AutoShootUntilEmpty())
-//     );
-//   }
-// }
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.DeferredCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.RobotContainer;
+import frc.robot.lib.TrajectoryHelper;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class AutoStrategyOne extends SequentialCommandGroup {
+  /** Creates a new AutoStrategyOne. */
+  public AutoStrategyOne() {
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    addCommands(
+      RobotContainer.runTrajectory2Poses(
+                RobotContainer.questNavSubsystem.getQuestRobotPose2d(),
+                TrajectoryHelper.AutoDesiredPoses.BlueDepot,
+                false)
+            .alongWith(new AutoShootUntilEmpty()),
+      new StartIntake()
+      .raceWith(new WaitCommand(3)),
+      new StopIntake(),
+      RobotContainer.runTrajectory2Poses(
+                RobotContainer.questNavSubsystem.getQuestRobotPose2d(),
+                TrajectoryHelper.AutoDesiredPoses.BlueTower,
+                false)
+            .alongWith(new AutoShootUntilEmpty())
+      
+    );
+  }
+}
