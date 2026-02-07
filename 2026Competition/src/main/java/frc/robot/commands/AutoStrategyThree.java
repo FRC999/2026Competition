89bae6478fd5a6c6edf4fbf73af30585279d20f3
Sudhamsu <academyoffice79@gmail.com>
2026-02-07 14:12:55 -0500

    Started Auto Strat 3

--- 2026Competition/src/main/java/frc/robot/commands/AutoStrategyThree.java
+++ 2026Competition/src/main/java/frc/robot/commands/AutoStrategyThree.java
@@ -5,17 +5,18 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.RobotContainer;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class AutoStrategy2 extends SequentialCommandGroup {
+public class AutoStrategyThree extends SequentialCommandGroup {
   /** Creates a new AutoStrategy2. */
-  public AutoStrategy2() {
+  public AutoStrategyThree() {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
-      
+      RobotContainer.runTrajectory2Poses(null, null, isScheduled())
     );
   }
 }
