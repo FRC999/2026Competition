62e56aa10c5672ba7930bddeb9eb04decead1b98
Sudhamsu <academyoffice79@gmail.com>
2026-02-08 13:26:54 -0500

    added motor to hopper system

--- 2026Competition/src/main/java/frc/robot/subsystems/HopperSubsystem.java
+++ 2026Competition/src/main/java/frc/robot/subsystems/HopperSubsystem.java
@@ -2,8 +2,18 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.MotionMagicDutyCycle;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants;
+import frc.robot.Constants.OperatorConstants.IntakeConstants;
 
 /**
  * HopperSubsystem
@@ -21,17 +31,32 @@ public class HopperSubsystem extends SubsystemBase {
 
   private boolean extended = false;
 
-  public HopperSubsystem() {}
+  private static TalonFX hopperExtendMotor;
+
+  public HopperSubsystem() {
+    hopperExtendMotor = new TalonFX(0);
+  }
+
+  private void configureMotors() { 
+    hopperExtendMotor.getConfigurator().apply(new TalonFXConfiguration());
+    hopperExtendMotor.setSafetyEnabled(false);
+
+    var hopperMotorConfig = new MotorOutputConfigs();
+    hopperMotorConfig.NeutralMode = NeutralModeValue.Brake;
+    // hopperMotorConfig.Inverted = (HopperConstants.hopperMotorInverted ? InvertedValue.CounterClockwise_Positive: InvertedValue.Clockwise_Positive);
+  }
 
   /** Extend the hopper (placeholder). */
   public void extend() {
     extended = true;
+
   }
 
   /** Retract the hopper (placeholder). */
   public void retract() {
     extended = false;
   }
+
 
   /** @return true if hopper is currently extended (placeholder). */
   public boolean isExtended() {
