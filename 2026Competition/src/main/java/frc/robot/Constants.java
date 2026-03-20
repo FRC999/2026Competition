8839a33db1869791b77f784a3fe68c2929a0e9cf
Sudhamsu <academyoffice79@gmail.com>
2026-03-19 20:46:48 -0400

    constants

--- 2026Competition/src/main/java/frc/robot/Constants.java
+++ 2026Competition/src/main/java/frc/robot/Constants.java
@@ -1217,7 +1217,7 @@ public final class Constants {
       public static final double CAL_STEP_LOW_DEG = 30.0;
       public static final double CAL_STEP_HIGH_DEG = 90.0;
       public static final double INTAKE_PIVOT_POWER_OUT_DUTY = 0.20;
-      public static final double INTAKE_PIVOT_POWER_OUT_TIME_SEC = 0.1;
+      public static final double INTAKE_PIVOT_POWER_OUT_TIME_SEC = 0.5;
       public static final double INTAKE_PIVOT_POWER_IN_TIME_SEC = 0.5;
 
       public static enum IntakePositions { // arm degrees (not motor rotations)
