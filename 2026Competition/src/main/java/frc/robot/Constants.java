f537df0b81cba4b17ffb73549c92e6bdf3abea5d
Sudhamsu <academyoffice79@gmail.com>
2026-03-21 11:34:31 -0400

    Quals 2 Commit

--- 2026Competition/src/main/java/frc/robot/Constants.java
+++ 2026Competition/src/main/java/frc/robot/Constants.java
@@ -125,7 +125,7 @@ public final class Constants {
 
   public static final class DebugTelemetrySubsystems {
     public static final boolean odometry = true;
-    public static final boolean imu = false;
+    public static final boolean imu = true;
     public static final boolean chassis = false;
     public static final boolean ll = false;
     public static final boolean questnav = false;
