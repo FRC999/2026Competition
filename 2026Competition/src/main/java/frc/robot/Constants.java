28442b69aa81ac10daf1311441d56a82203de6c7
Sudhamsu <academyoffice79@gmail.com>
2026-04-23 19:31:00 -0400

    reenable quest

--- 2026Competition/src/main/java/frc/robot/Constants.java
+++ 2026Competition/src/main/java/frc/robot/Constants.java
@@ -125,11 +125,11 @@ public final class Constants {
 
   public static final class DebugTelemetrySubsystems {
     public static final boolean odometry = true;
-    public static final boolean imu = false;
+    public static final boolean imu = true;
     public static final boolean chassis = true;
     public static final boolean ll = false;
     public static final boolean llLight = false;
-    public static final boolean questnav = false;
+    public static final boolean questnav = true;
     public static final boolean perfLight = false;
     public static final boolean intake = true;
     public static final boolean shooter = true;
